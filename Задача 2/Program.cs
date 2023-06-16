c786a91dd3eedfc1e4172e6bc770c20be90ff993
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-16 17:11:35 +0300

    выполнили задачу 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+string? input1 = Console.ReadLine();
+int num1 = int.Parse(input1!);
+
+Console.Write("Введите второе число: ");
+string? input2 = Console.ReadLine();
+int num2 = int.Parse(input2!);
+
+if(num1>num2) {
+    System.Console.WriteLine($"{num1} большее число");
+} else {
+    System.Console.WriteLine($"{num2} большее число");
+}
