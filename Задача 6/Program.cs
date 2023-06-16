389f3ee23e8467b6f2ee1199609a0694904c09e4
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-16 18:21:16 +0300

    выполнили задачу 6

---
+++ Задача 6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+string? input1 = Console.ReadLine();
+int num1 = int.Parse(input1!); 
+if  (num1 % 2 == 0) {
+   System.Console.WriteLine($"{num1} четное");
+} else {
+   System.Console.WriteLine($"{num1} нечетное"); 
+}
+
